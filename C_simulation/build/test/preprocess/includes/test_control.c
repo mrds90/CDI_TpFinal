cca399a33d507e49bfd5b1024834dd0519d22d0f
Marcos Dominguez <mrds0690@gmail.com>
2024-05-27 17:00:54 -0300

    feat: ignore build folder

--- C_simulation/build/test/preprocess/includes/test_control.c
+++
@@ -1,7 +0,0 @@
----
-- "/var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
-- src/Control/control.h
-- src/RealWorld/real_world.h
-- src/OSAL/inc/osal_task.h
-- src/OSAL/inc/port/port_task_freertos.h
-- src/OSAL/inc/port/support/FreeRTOS_task_simulated.h
--- C_simulation/tools/simulacion_planta.py
+++ C_simulation/tools/simulacion_planta.py
@@ -3,9 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def SquareFunc(fs: float, f0: float, amp: float = 1.0, samples: int = 100) -> [np.ndarray, np.ndarray]:
-    t = np.arange(0, samples) / fs
-    square = (amp * np.sign(np.sin(2 * np.pi * f0 * t)))
-    return t, square
+    t_step = np.arange(0, samples) / fs
+    square = (amp * np.sign(np.sin(2 * np.pi * f0 * t_step)))
+    square[square < 0] = 0
+    return t_step, square
 
 # Obtiene la ruta del archivo actual
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,10 +20,16 @@ expected_output_path = os.path.join(expected_output_dir, "expected_output.h")
 NUM = [int(0.00067056 * (1 << 15)), int(0.00064738 * (1 << 15))]
 DEN = [int(1.0 * (1 << 15)), int(-1.898506534800089 * (1 << 15)), int(0.8998244812091836 * (1 << 15))]
 
+h = 0.005
 # Buffers para mantener el estado
 input_buffer = [0] * len(NUM)
 output_buffer = [0] * (len(DEN) - 1)
 
+def reset_recurrence():
+    global input_buffer, output_buffer
+    input_buffer = [0] * len(NUM)
+    output_buffer = [0] * (len(DEN) - 1)
+    
 # Función de recurrencia adaptada a Python
 def recurrence_function(input_value):
     global input_buffer, output_buffer
@@ -48,17 +55,40 @@ def recurrence_function(input_value):
 
     return output
 
+
+
 # Simulación en Python
-t, u = SquareFunc(fs=1/0.005, f0=(1), amp=2**15, samples=200)
+# t_step, u_step = SquareFunc(fs=1/h, f0=(10), amp=2**15, samples=(1/h))
+t_step = np.arange(0,6,h)
+u_step = np.concatenate((np.zeros(int(len(t_step) / 2)), 2**15 * np.ones(int(len(t_step) / 2))), axis=None)
 
 # Vector para almacenar la salida
-y_1 = []
+y_step = []
 
-for value in u:
-    y_1.append(recurrence_function(int(value)))
+for value in u_step:
+    y_step.append(recurrence_function(int(value)))
 
 # Convertir a numpy array para facilitar el ploteo
-y_1 = np.array(y_1)
+y_step = np.array(y_step)
+step_input_str = f"static int32_t step_input[] = {{{', '.join(map(str, u_step.astype(int)))}}};\n"
+step_output_str = f"static int32_t step_expected_output[] = {{{', '.join(map(str, y_step))}}};\n\n"
+
+reset_recurrence()
+
+t_square, u_square = SquareFunc(fs=1/h, f0=(10), amp=9930, samples=10*(1/h))
+u_square+= 9929
+u_square=u_square[1:]
+t_square=t_square[1:]
+
+y_square = []
+
+for value in u_square:
+    y_square.append(recurrence_function(int(value)))
+
+# Convertir a numpy array para facilitar el ploteo
+y_square = np.array(y_square)
+square_input_str = f"static int32_t square_input[] = {{{', '.join(map(str, u_square.astype(int)))}}};\n"
+square_output_str = f"static int32_t square_expected_output[] = {{{', '.join(map(str, y_square))}}};\n\n"
 
 # Encabezado del archivo
 header_str = '''/**
@@ -101,19 +131,29 @@ footer_str = '''
 '''
 
 # Guardar la salida y la entrada en el formato deseado
-output_str = f"static int32_t expected_output[] = {{{', '.join(map(str, y_1))}}};\n"
-input_str = f"static int32_t input[] = {{{', '.join(map(str, u.astype(int)))}}};\n"
+
 
 # Escribir los datos en un archivo
 with open(expected_output_path, "w") as f:
     f.write(header_str)
-    f.write(output_str)
-    f.write(input_str)
+    f.write(step_input_str)
+    f.write(step_output_str)
+    f.write(square_input_str)
+    f.write(square_output_str)
     f.write(footer_str)
 
 # Graficar la salida
-plt.plot(t, y_1, label="output")
-plt.plot(t, u, label="input")
+plt.plot(t_step, y_step, label="output")
+plt.plot(t_step, u_step, label="input")
+plt.title("Output of Recurrence Function")
+plt.xlabel("Time")
+plt.ylabel("Output (Q15)")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+plt.plot(t_square, y_square, label="output")
+plt.plot(t_square, u_square, label="input")
 plt.title("Output of Recurrence Function")
 plt.xlabel("Time")
 plt.ylabel("Output (Q15)")
